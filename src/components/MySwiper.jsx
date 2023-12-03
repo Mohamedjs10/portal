847790e74caf8817c50d73c9bee01574bb7eb9b4
Mohamed Ibrahim <moibrahim@contact.eg>
2023-12-03 09:25:42 +0200

    update

--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -18,6 +18,7 @@ const news = [
     title: { en: "", ar: "الرياض في قلب العالم" },
     description: { en: "", ar: "الرياض اكسبو 2023، ملتقى لكل شعوب العالم" },
     url: "https://i.postimg.cc/85hwCsjH/0852383d-9c67-4773-aeeb-b0e70cbb531f.jpg",
+    img: "/01.jpg",
   },
   {
     title: { en: "", ar: "خدمة السحابة الحكومية (ديم)" },
@@ -26,6 +27,7 @@ const news = [
       ar: "سحابة حكومية تتيح للمستخدمين رفع الملفات ومزامنتها في سحابة التخزين ومن ثم الوصول اليها من خلال الكمبيوتر او المتصفح او الهواتف المتنقلة من أي مكان وفي أي وقت.",
     },
     url: "https://i.postimg.cc/hjLj3R5v/0852383d-9c67-4773-aeeb-b0e70cbb531f.jpg",
+    img: "/02.jpg",
   },
   {
     title: { en: "", ar: "سحابة ديم في ليب" },
@@ -34,6 +36,7 @@ const news = [
       ar: "تعني السحابة الحكومية ديم بتوقير خدمات الأصول التقنية لجميع الجهات الحكومية بكل أمان واعتمادية ومرونة، وتتيح الاستفادة منها بشكل لحظي ومباشر.",
     },
     url: "https://i.postimg.cc/SKT6g8Hj/0852383d-9c67-4773-aeeb-b0e70cbb531f.jpg",
+    img: "/03.jpg",
   },
 ];
 export default function MySwiper() {
@@ -64,14 +67,14 @@ export default function MySwiper() {
                 height: "100%",
                 color: mode === "light" ? "#0f3a62" : "#fff",
                 backgroundColor: mode === "light" ? "#fff" : "#0f3a62",
-                padding: "10px",
-                paddingTop: "30px",
+                // padding: "10px",
+                // paddingTop: "30px",
               }}
               onClick={() => {
                 window.open(item.url, "_blank").focus();
               }}
             >
-              <div
+              {/* <div
                 style={{
                   fontWeight: "bold",
                   fontSize: "25px",
@@ -82,7 +85,8 @@ export default function MySwiper() {
               </div>
               <div>
                 {locale === "en" ? item.description.en : item.description.ar}
-              </div>
+              </div> */}
+              <img src={item.img} />
             </div>
           </SwiperSlide>
         ))}
